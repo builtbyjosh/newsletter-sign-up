ce96eaa3974afa9e4cd365b1f2d69e72a69afbda
builtbyjosh <joshuacollins912@gmail.com>
2023-07-11 16:44:58 -0500

    Most styling and form control

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import illistration from "./images/illustration-sign-up-desktop.svg";
-
+import { useForm } from "react-hook-form";
 function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div className="App">
-      <div>
+      <div className="Info">
         <div>
           <h1>Stay updated!</h1>
           <p>Join 60,000+ product managers receiving monthly updates on:</p>
@@ -15,17 +24,29 @@ function App() {
           </ul>
         </div>
         <div>
-          <p>Email address</p>
-          <input
-            type="text"
-            name="email"
-            id="email"
-            placeholder="email@company.com"
-          />
-          <button type="submit">Subscribe to monthly newsletter</button>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="InputLabel">
+              <label>Email address</label>
+              {errors.email && (
+                <label className="errorLabel">{errors.email.message}</label>
+              )}
+            </div>
+            <input
+              id="email"
+              placeholder="email@company.com"
+              {...register("email", {
+                required: "required",
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: "Entered value does not match email format",
+                },
+              })}
+            />
+            <button type="submit">Subscribe to monthly newsletter</button>
+          </form>
         </div>
       </div>
-      <div>
+      <div className="Image">
         <img src={illistration} />{" "}
       </div>
     </div>
