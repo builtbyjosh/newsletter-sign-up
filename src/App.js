bf92dccf43d35dc4d5aa8b568e1a9c26dbcf8273
builtbyjosh <joshuacollins912@gmail.com>
2023-07-12 13:23:57 -0500

    add sucess mesasge

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import "./App.css";
 import illistration from "./images/illustration-sign-up-desktop.svg";
+import check from "./images/icon-success.svg";
 import { useForm } from "react-hook-form";
 function App() {
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [email, setEmail] = useState("");
   const {
     register,
     handleSubmit,
@@ -10,46 +14,64 @@ function App() {
   } = useForm();
   const onSubmit = (data) => {
     console.log(data);
+    setEmail(data.email);
+    setIsSuccess(true);
   };
   return (
-    <div className="App">
-      <div className="Info">
-        <div>
-          <h1>Stay updated!</h1>
-          <p>Join 60,000+ product managers receiving monthly updates on:</p>
-          <ul>
-            <li>Product discovery and building what matters</li>
-            <li>Measuring to ensure updates are a success</li>
-            <li>And much more!</li>
-          </ul>
-        </div>
-        <div>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="InputLabel">
-              <label>Email address</label>
-              {errors.email && (
-                <label className="errorLabel">{errors.email.message}</label>
-              )}
+    <>
+      {!isSuccess && (
+        <div className="App">
+          <div className="Info">
+            <div>
+              <h1>Stay updated!</h1>
+              <p>Join 60,000+ product managers receiving monthly updates on:</p>
+              <ul>
+                <li>Product discovery and building what matters</li>
+                <li>Measuring to ensure updates are a success</li>
+                <li>And much more!</li>
+              </ul>
             </div>
-            <input
-              id="email"
-              placeholder="email@company.com"
-              {...register("email", {
-                required: "required",
-                pattern: {
-                  value: /\S+@\S+\.\S+/,
-                  message: "Entered value does not match email format",
-                },
-              })}
-            />
-            <button type="submit">Subscribe to monthly newsletter</button>
-          </form>
+            <div>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="InputLabel">
+                  <label>Email address</label>
+                  {errors.email && (
+                    <label className="errorLabel">{errors.email.message}</label>
+                  )}
+                </div>
+                <input
+                  id="email"
+                  placeholder="email@company.com"
+                  {...register("email", {
+                    required: "required",
+                    pattern: {
+                      value: /\S+@\S+\.\S+/,
+                      message: "Entered value does not match email format",
+                    },
+                  })}
+                />
+                <button type="submit">Subscribe to monthly newsletter</button>
+              </form>
+            </div>
+          </div>
+          <div className="Image">
+            <img src={illistration} />{" "}
+          </div>
         </div>
-      </div>
-      <div className="Image">
-        <img src={illistration} />{" "}
-      </div>
-    </div>
+      )}
+      {isSuccess && email && (
+        <div className="successMsg">
+          <img src={check} alt="" />
+          <h1>Thanks for subscribing!</h1>
+          <p>
+            A confirmation email has been sent to <strong>{email}</strong>.
+            Please open it and click the button inside to confirm your
+            subscription.
+          </p>
+          <button onClick={() => setIsSuccess(false)}>Dismiss message</button>
+        </div>
+      )}
+    </>
   );
 }
 
